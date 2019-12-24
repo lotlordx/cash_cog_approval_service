11a5c7d5ae697cc9064881a07612d2020a4770cf
Lotanna <pythonchal@gmail.com>
2019-12-24 14:49:03 +0100

    added comments to the currency model

--- server_be/app/currency_app/models.py
+++ server_be/app/currency_app/models.py
@@ -2,8 +2,13 @@ from app import db
 
 
 class CurrencyModel(db.Model):
+    """
+    This represents the currency Model / currency table.
+    Field attributes declared here represents , corresponding
+    column names and attributes in the db.
+    """
 
-    __tablename__ = 'currency_model'
+    __tablename__ = "currency_model"
 
     id = db.Column(db.Integer, primary_key=True)
     currency_code = db.Column(db.String(5), unique=True)
@@ -12,6 +17,10 @@ class CurrencyModel(db.Model):
         self.currency_code = currency
 
     def save(self):
+        """
+        This function saves a currencyModel object to the db
+        """
+
         try:
             db.session.add(self)
             db.session.commit()
@@ -20,4 +29,7 @@ class CurrencyModel(db.Model):
 
     @staticmethod
     def roll_back():
+        """
+        This function rolls back a commit state in the db
+        """
         db.session.rollback()

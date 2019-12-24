50b48ff9ea4c026c8f2b87b02127a7fecd1e4492
Lotanna <pythonchal@gmail.com>
2019-12-24 14:52:07 +0100

    added comments to the currency schema

--- server_be/app/currency_app/schema.py
+++ server_be/app/currency_app/schema.py
@@ -3,5 +3,11 @@ from app import ma
 
 
 class CurrencySchema(ma.ModelSchema):
-        class Meta:
-            model = CurrencyModel
+    """
+    This CLass Handles the serialization and deserialization
+    of the currencymodel instance. Flask Marshamallow lib
+    ModelSchema Class was Inherited to perform this
+    action.
+    """
+    class Meta:
+        model = CurrencyModel

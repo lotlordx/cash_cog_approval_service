0247b325eadd4a3dcb505a9ceaefa90e503ea805
Lotanna <pythonchal@gmail.com>
2019-12-24 16:52:14 +0100

    created the run entry point of the application

---
+++ server_be/run.py
@@ -0,0 +1,28 @@
+from flask_restful import Api
+from flask_cors import CORS
+
+from app.employee_app.controller import EmployeeController
+from settings.constants import ENVIROMENT
+from app import create_app, make_celery
+from app.transaction_app.controller import TransactionListController, TransactionController, \
+    TransactionAnalyticsController
+from app.currency_app.controller import CurrencyController
+
+
+app = create_app(ENVIROMENT)
+celery = make_celery(app)
+CORS(app)
+
+
+rest_route = Api(app, '/api/v1')
+rest_route.add_resource(TransactionListController, '/transactions')
+rest_route.add_resource(TransactionController, '/transactions/<trans_id>')
+rest_route.add_resource(TransactionAnalyticsController, '/transactions/analytics')
+rest_route.add_resource(CurrencyController, '/currency')
+rest_route.add_resource(EmployeeController, '/employee')
+
+if __name__ == "__main__":
+    from tools.celery.tasks import kafka_producer
+    kafka_producer.delay()
+
+    app.run(host='0.0.0.0')
